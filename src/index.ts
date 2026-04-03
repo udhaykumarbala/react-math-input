66d48fbc2893b02cfe9ec29c9728f5dbc1aa3694
udhaykumarbala <udhayk97@gmail.com>
2026-04-03 15:25:59 +0200

    feat: controlled/uncontrolled mode, form integration, size/disabled/readOnly/error props

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ export { default as MathRenderer, renderLatexString } from "./MathRenderer";
 export { default as QuestionForm } from "./QuestionForm";
 export { SYMBOL_CATEGORIES } from "./symbols";
 
-export type { MathEditorRef } from "./MathEditor";
+export type { MathEditorRef, MathEditorProps } from "./MathEditor";
 export type { MathSymbol } from "./symbols";
 export type { QuestionPayload, QuestionOption } from "./QuestionForm";
